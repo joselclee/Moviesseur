4a714403cb88fa41c0e6442e35f15afc254e4496
LevelAndre <andrelee089@gmail.com>
2023-11-01 18:59:26 -0700

    Back End Code
    Maven Application and REST API for MongoDB

---
+++ movies/src/main/java/me/andrelee/movies/MoviesApplication.java
@@ -0,0 +1,12 @@
+package me.andrelee.movies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesApplication.class, args);
+	}
+}
